b9344f7054abee8f4b771140af9ffce201448777
02705534288 <leandronascimento@lampp-it.com.br>
2024-12-12 13:30:29 -0500

    finalizando estilização da aplicação

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -16,7 +16,7 @@ export function Comment() {
                         </div>
 
                         <button title="Deletar comentário">
-                            <Trash size={20}></Trash>
+                            <Trash size={24}></Trash>
                         </button>
                     </header>
 
